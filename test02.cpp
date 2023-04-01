2172aa77a82189e7c56ca5a5446332f1c061e1b0
TOBY <hzyiunn@gmail.com>
2023-04-02 00:50:40 +0900

    같은 숫자는 싫어
    효율성 실패 코드와 효율성 성공한 코드

---
+++ test02.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    answer.push_back(arr[0]);
+    for(int i = 0 ; i < arr.size()-1;)
+    {
+        if(arr[i] == arr[i+1])
+        {
+            arr.erase(arr.begin() + i);
+        }
+        else
+        {
+            i++;
+            answer.push_back(arr[i]);
+        }
+    }
+    return answer;
+}
