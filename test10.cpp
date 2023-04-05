f8f96358f3a73e8cac8a2e0efc1ed8ac21dd38e4
TOBY <hzyiunn@gmail.com>
2023-04-05 23:08:35 +0900

    정수 제곱근 판별

---
+++ test10.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    int num = sqrt(n);
+    if( pow(num, 2) == n)
+    {
+        answer = pow(num+1,2);
+    }
+    else if(n == 1)
+    {
+        answer = 4;
+    }
+    else
+    {
+        answer = -1;
+    }
+    return answer;
+}
