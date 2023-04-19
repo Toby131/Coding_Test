4f1c784c478bebeed041f5c579e54a9b9841e48a
TOBY <hzyiunn@gmail.com>
2023-04-19 15:53:11 +0900

    [3차] 문제5) 전광판 문구 출력 - JAVA

---
+++ COS_PRO1/test18.java
@@ -0,0 +1,28 @@
+// 다음과 같이 import를 사용할 수 있습니다.
+import java.util.*;
+
+class Main {
+    public String solution(String phrases, int second) {
+        // 여기에 코드를 작성해주세요.
+			String answer = "";
+			String dash = "_";
+			answer = dash.repeat(14-second);
+      if(second > 14){
+				answer += phrases.substring(0, second%14);
+			}
+			else{
+				answer += phrases.substring(0, second);
+			}
+        return answer;
+    }
+    
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Main sol = new Main();
+        String phrases = new String("happy-birthday");
+        int second = 3;
+        String ret = sol.solution(phrases, second);
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 \"" + ret + "\" 입니다.");
+    }
+}
