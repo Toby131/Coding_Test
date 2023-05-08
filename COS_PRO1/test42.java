bab1727f9f4e2823a0f877750d6856fef204c626
hzyiunn <hzyiunn@gmail.com>
2023-05-08 15:43:27 +0900

    [2차] 문제6) 로봇을 움직여주세요 - JAVA

--- COS_PRO1/test42.java
+++ COS_PRO1/test42.java
@@ -2,29 +2,38 @@
 import java.util.*;
 
 class Main {
-    public int solution(int[] arr) {
-			int answer = 0;
-			int count = 0;
-			int min = arr[0];
-			for(int i = 1 ; i < arr.length ; i++){
-				if(arr[i] > arr[i-1]) count++;
-				else{
-					answer = Math.max(answer, count);
-					count = 1;
-				} 
-			}
-
-			if(answer < 2) return 1;
-      return answer;
+    public int[] solution(String commands) {
+        
+        int[] answer = new int[2];
+			  int x = 0;
+				int y = 0;
+			  String[] arr = commands.split("");
+			  for(int i = 0 ; i < arr.length; i++){
+					if(arr[i].equals("L")){
+						x -= 1;
+					}
+					else if(arr[i].equals("R")){
+						x += 1;
+					}
+					else if(arr[i].equals("U")){
+						y += 1;
+					}
+				  else if(arr[i].equals("D")){
+						y -= 1;
+					}
+				}
+		  answer[0] = x;
+			answer[1] = y;
+			return answer;
     }
 
     // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
-        int[] arr = {3, 1, 2, 4, 5, 1, 2, 2, 3, 4};
-        int ret = sol.solution(arr);
+        String commands = "URDDL";
+        int[] ret = sol.solution(commands);
 
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
     }
 }
