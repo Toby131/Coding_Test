1612b9e8d1788f04f8b347345869e4592b569c55
hzyiunn <hzyiunn@gmail.com>
2023-05-10 14:09:44 +0900

    올바른 괄호

---
+++ JAVA/test03.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<String> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.substring(i,i+1).charAt(0) == '(')
+                stack.push("(");
+            else{
+                if(stack.empty())
+                    return false;
+                else stack.pop();
+            }
+        }
+        
+        if(!stack.empty())
+            return false;
+        return answer;
+    }
+}
