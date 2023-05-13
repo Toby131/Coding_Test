c22bea9c3dc34f39723ad7408a7bcf6b839668ea
hzyiunn <leehj94131@gmail.com>
2023-05-14 00:13:26 +0900

    제일 작은 수 제거하기

--- cpp/test11.cpp
+++ cpp/test11.cpp
@@ -6,29 +6,22 @@ using namespace std;
 vector<int> solution(vector<int> arr) {
     vector<int> answer;
     int mini = arr[0];
+    
     if(arr.size() < 2)
-    {
         answer.push_back(-1);
-    }
     else
     {
         for(int i = 0 ; i < arr.size() ; i++)
         {
             if(mini > arr[i])
-            {
                 mini = arr[i];
-            }
             else
-            {
                 continue;
-            }
         }
         for(int k = 0 ; k < arr.size() ; k++)
         {
             if(arr[k] == mini)
-            {
                 arr.erase(arr.begin() + k);
-            }
         }
         answer = arr;
     }
