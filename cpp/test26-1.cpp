3f9e52751b90128beb16378021ac7c1235ef5dfa
hzyiunn <leehj94131@gmail.com>
2023-05-14 00:11:09 +0900

    문자열 다루기 기본1

--- cpp/test26-1.cpp
+++ cpp/test26-1.cpp
@@ -8,9 +8,7 @@ bool solution(string s) {
     for(int i = 0 ; i < s.size(); i++)
     {
         if((s.size() == 4 || s.size() == 6)&& isdigit(s[i]))
-        {
             answer = true;
-        }
         else
         {
             answer = false;
