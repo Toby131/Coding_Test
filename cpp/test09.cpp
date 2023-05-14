6f67cc3eef184960fae0ff4485f784e5428ec956
hzyiunn <leehj94131@gmail.com>
2023-05-14 22:55:09 +0900

    정수 내림차순으로 배치하기

--- cpp/test09.cpp
+++ cpp/test09.cpp
@@ -6,7 +6,7 @@ using namespace std;
 long long solution(long long n) {
     long long answer = 0;
     string temp = to_string(n);
-    sort(temp.begin(), temp.end(), greater<char>()); //string 873211
+    sort(temp.begin(), temp.end(), greater<char>());
     answer = stoll(temp);
     return answer;
 }
