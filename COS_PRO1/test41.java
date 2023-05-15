14a9c898c086f9d33fa96176d413f693278730ad
hzyiunn <leehj94131@gmail.com>
2023-05-15 13:45:31 +0900

    체스의 나이트

--- COS_PRO1/test41.java
+++ COS_PRO1/test41.java
@@ -1,20 +1,25 @@
 class Main {
     public int solution(String pos) {
         
-        int answer = 0;
-			  int posFirst = pos.substring(0).charAt(0)-64;
-			  int posSecond = Integer.parseInt(pos.substring(1,pos.length()));
-			  
-			if(posFirst-2 >= 0 && posSecond -1 >= 0) answer++;
-			if(posFirst-2 >= 0 && posSecond +1 >= 0 && posSecond +1 < 8) answer++;
-			if(posFirst-1 >= 0 && posSecond -2 >= 0) answer++;
-			if(posFirst-1 >= 0 && posSecond +2 >= 0 && posSecond +2 < 8) answer++;
-			if(posFirst+1 >= 0 && posFirst+1 < 8 && posSecond -2 >= 0) answer++;
-			if(posFirst+1 >= 0 && posFirst+1 < 8 && posSecond +2 >= 0 && posSecond +2 < 8) answer++;
-			if(posFirst+2 >= 0 && posFirst+2 < 8 && posSecond -1 >= 0) answer++;
-			if(posFirst+2 >= 0 && posFirst+2 < 8 && posSecond +1 >= 0 && posSecond +1 < 8) answer++;
-			 
-    return answer;
+      int answer = 0;
+			
+			int[][] arr = new int[8][8];
+			
+			int[] dx = {2, 2, 1 ,1, -1, -1, -2, -2};
+			int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
+			
+			int posA = pos.charAt(0) - 'A';
+			int posB = pos.charAt(1) - '0' -1;
+			
+			for(int i = 0 ; i < 8 ; i++){
+				int x = dx[i] + posA;
+				int y = dy[i] + posB;
+				if( x>= 0 && y >= 0 && x < 8 && y < 8){
+					answer++;
+				}
+			}
+			
+      return answer;
     }
         // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
