5b649d8fefd5e51fb575231732721d8f32c39e24
hzyiunn <leehj94131@gmail.com>
2023-05-15 14:09:11 +0900

    [1차] 문제6) 체스의 나이트

--- COS_PRO1/test41.java
+++ COS_PRO1/test41.java
@@ -2,25 +2,25 @@ class Main {
     public int solution(String pos) {
         
       int answer = 0;
+      int[][] arr = new int[8][8];
 			
-			int[][] arr = new int[8][8];
+      int[] dx = {2, 2, 1 ,1, -1, -1, -2, -2};
+      int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
 			
-			int[] dx = {2, 2, 1 ,1, -1, -1, -2, -2};
-			int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
+      int posA = pos.charAt(0) - 'A';
+      int posB = pos.charAt(1) - '0' -1;
 			
-			int posA = pos.charAt(0) - 'A';
-			int posB = pos.charAt(1) - '0' -1;
-			
-			for(int i = 0 ; i < 8 ; i++){
-				int x = dx[i] + posA;
-				int y = dy[i] + posB;
-				if( x>= 0 && y >= 0 && x < 8 && y < 8){
-					answer++;
-				}
-			}
-			
+      for(int i = 0 ; i < 8 ; i++){
+            int x = dx[i] + posA;
+            int y = dy[i] + posB;
+            if( x>= 0 && y >= 0 && x < 8 && y < 8){
+                  answer++;
+            }
+      }
+	    
       return answer;
     }
+	
         // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Main sol = new Main();
