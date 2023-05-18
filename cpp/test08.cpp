0bf6b00f612d16b72ec9d3212e96fe543e5c4a1a
hzyiunn <leehj94131@gmail.com>
2023-05-18 09:36:08 +0900

    자연수 뒤집어 배열로 만들기

--- cpp/test08.cpp
+++ cpp/test08.cpp
@@ -6,6 +6,7 @@ using namespace std;
 vector<int> solution(long long n) {
     vector<int> answer;
     string s = to_string(n);
+    
     for(int i = s.size() ; i > 0 ; i--)
     {
         answer.push_back(s[i-1]-'0');
