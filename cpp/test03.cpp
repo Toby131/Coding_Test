ddfe28882238b407fb0abc143954dc5a948a766f
hzyiunn <leehj94131@gmail.com>
2023-05-24 21:36:39 +0900

    같은 숫자는 싫어

--- cpp/test03.cpp
+++ cpp/test03.cpp
@@ -8,13 +8,9 @@ vector<int> solution(vector<int> arr)
     for(int i = 0 ; i < arr.size()-1;i++)
     {
         if(arr[i] == arr[i+1])
-        {
             continue;
-        }
         else
-        {
             answer.push_back(arr[i]);
-        }
     }
     answer.push_back(arr[arr.size() -1 ]);
     return answer;
