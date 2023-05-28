e88d286aa181d7d5d0b3ba2c54e7bc1d054a9df4
hzyiunn <leehj94131@gmail.com>
2023-05-28 13:51:20 +0900

    폰켓몬

--- cpp/test01.cpp
+++ cpp/test01.cpp
@@ -9,14 +9,9 @@ int solution(vector<int> nums)
     for (int i = 0; i < nums.size() - 1;)
     {
         if (nums[i] == nums[i + 1])
-        {
             nums.erase(nums.begin() + i + 1);
-   
-        }
         else
-        {
             i++;
-        }
     }
     int maximum = nums.size();
     return min(minimum, maximum);
