ef2369020874f335fff0d0bcc7735c039130da7b
hzyiunn <leehj94131@gmail.com>
2023-06-11 17:51:27 +0900

    루시와 엘라 찾기

--- SQL/test10.sql
+++ SQL/test10.sql
@@ -2,5 +2,5 @@
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME = 'Lucy' OR NAME = 'Ella' OR NAME = 'Pickle'
-        OR NAME = 'Rogan' OR NAME = 'Sabrina' OR NAME = 'Mitty'
+   OR NAME = 'Rogan' OR NAME = 'Sabrina' OR NAME = 'Mitty'
 ORDER BY ANIMAL_ID
