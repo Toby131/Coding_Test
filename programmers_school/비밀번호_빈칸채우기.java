e4c670f31ea07234e4592e19a798ca90a4870405
hzyiunn <hzyiunn@gmail.com>
2023-09-17 20:26:26 +0900

    Add files via upload

---
+++ programmers_school/비밀번호_빈칸채우기.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+class Solution {
+    public int solution(String[] password) {
+        int valid_count = 0;
+
+        for (String pw : password) {
+            boolean[] char_used = new boolean[26];  
+
+            for (char c : pw.toCharArray()) {
+                int index = c - 'a';
+                
+char_used[index]
+ = true;
+            }
+
+            int unique_char_count = 0;
+            for (boolean used : char_used) {
+                if (used) {
+                    unique_char_count++;
+                }
+            }
+
+            if (unique_char_count >= 5) {
+                
+valid_count++
+;
+            }
+        }
+
+        return valid_count;
+    }
+}
